6c9e53b5c455a84dc91302173b05cfacfa3f7ccd
Ogooluwa Olutimilehin <ogooluwanick@gmail.com>
2022-09-07 18:33:59 +0100

    laravel first learn commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="logo icon.png">
+        <title>Posty</title>
+        @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-200">
+        <nav class="p-6 bg-white flex justify-between">
+                <ul class="flex items-center">
+                        <li>
+                                <a href="" class="p-3">HOME</a>
+                        </li>
+                        <li>
+                                <a href="" class="p-3">DASHBOARD</a>
+                        </li>
+                        <li>
+                                <a href="" class="p-3">POST</a>
+                        </li>
+                </ul>
+
+                <ul class="flex items-center">
+                        <li>
+                                <a href="" class="p-3">Ogooluwa Emmanuel</a>
+                        </li>
+                        <li>
+                                <a href="" class="p-3">Login</a>
+                        </li>
+                        <li>
+                                <a href="" class="p-3">Register</a>
+                        </li>
+                        <li>
+                                <a href="" class="p-3">Logout</a>
+                        </li>
+                </ul>
+        </nav>
+        @yield("content")
+</body>
+</html>
