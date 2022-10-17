10025921708c83a3f8950c02a173354ca02596e4
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-10-17 01:33:59 +0100

    run some migrations / created registration controller /worked on registration view

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,9 @@
+@extends("layouts.app")
+
+@section("content")
+        <div class="flex justify-center mt-5">
+                <div class=" bg-white p-6  rounded-md  w-4/12">
+                        Register
+                </div>
+        </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
+//Show index page 
+Route::get('/register',[RegisterController::class,"index" ]);
 
+//Show index page 
 Route::get('/posts', function () {
     return view('posts.index');
 });
