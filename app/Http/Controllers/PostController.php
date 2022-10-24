fcf52cf5610dbaee44d13ce3fe8857affc4ee1e5
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-10-24 22:36:42 +0100

    seeding model posts with factory

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(){
-        $posts=Post::latest()->paginate(5);
+        $posts=Post::latest()->simplePaginate(5);
         return view('posts.index',[
                 "posts"=>$posts
         ]);
